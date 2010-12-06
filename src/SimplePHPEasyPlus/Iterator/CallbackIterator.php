03efeb342b5c539c52730e55191ff4eb09152b8b
ornicar <thibault.duplessis@gmail.com>
2010-12-06 20:58:43 +0100

    Implement CallbackIterator

---
+++ src/SimplePHPEasyPlus/Iterator/CallbackIterator.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace SimplePHPEasyPlus\Iterator;
+use Closure;
+
+class CallbackIterator implements IteratorInterface
+{
+    protected $closure;
+
+    public function __construct(Closure $closure)
+    {
+        $this->closure = $closure;
+    }
+
+    public function iterate(IteratorableInterface $iteratorable)
+    {
+        foreach($iteratorable->getItems() as $item) {
+            $this->apply($item);
+        }
+    }
+
+    protected function apply($item)
+    {
+        $closure = $this->closure;
+        $closure($item);
+    }
+}
