afaaebe3dff01255908363188cf4974a5e829d9e
ornicar <thibault.duplessis@gmail.com>
2010-12-06 20:30:34 +0100

    Add addition functional test. It fails.

--- tests/Functional/AdditionTest.php
+++ tests/Functional/AdditionTest.php
@@ -2,8 +2,42 @@
 
 class AdditionTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * Verifies that 1 + 1 = 2
+     */
     public function testAddition()
     {
-        $this->assertTrue(true);
+        $numberCollectionGenerator = new NumberCollectionGenerator('NumberCollection');
+        $numberCollection = $numberCollectionGenerator->generate();
+
+        $numberReader = new NumberStringReader('1');
+        $number = new Number($numberReader);
+
+        $numberCollection->add(new Number(1));
+        $numberCollection->add(new Number(1));
+
+        $iterator = new CallbackIterator();
+        $addition = new Addition($iterator);
+
+        $operation = new ArithmeticOperation();
+        $operation->setOperator($addition);
+
+        $iterator = new CallbackIterator();
+        $operationStream = new OperationStream($iterator);
+        $operationStream->addOperation($operation);
+
+        $engine = new Engine();
+        $engine->setOperationStream($operationStream);
+
+        $calcul = new Calcul();
+        $calcul->setEngine($engine);
+        $calcul->setNumberCollection($numberCollection);
+
+        $runner = new CalculRunner();
+
+        $result = $runner->run($calcul);
+        $numericResult = $result->getNumericValue();
+
+        $this->assertEquals(2, $numericResult);
     }
 }
