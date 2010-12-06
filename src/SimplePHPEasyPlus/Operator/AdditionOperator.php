061c3608f62db56e05b8085f0c20397ad5cc34bc
ornicar <thibault.duplessis@gmail.com>
2010-12-06 22:25:59 +0100

    Add a #FIXME tag to AdditionOperator, fix call to NumberInterface::getValue

--- src/SimplePHPEasyPlus/Operator/AdditionOperator.php
+++ src/SimplePHPEasyPlus/Operator/AdditionOperator.php
@@ -6,9 +6,10 @@ class AdditionOperator extends CallbackOperator implements OperatorInterface
 {
     protected function atomicOperation(NumberInterface $numberOne, NumberInterface $numberTwo)
     {
+        #FIXME this is damn ugly! Sorry for the shameful hack
         $resultClass = get_class($numberOne);
 
-        $result = new $resultClass($numberOne->getNumericValue() + $numberTwo->getNumericValue());
+        $result = new $resultClass($numberOne->getValue() + $numberTwo->getValue());
 
         return $result;
     }
