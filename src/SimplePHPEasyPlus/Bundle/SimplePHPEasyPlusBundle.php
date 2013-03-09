f5d017b9788980785e9b6148af6a30dc9543e79b
Igor Wiedler <igor@wiedler.ch>
2013-03-09 19:26:16 +0100

    Add a Symfony2 bundle to integrate with the Symfony2 Dependency Injection Container
    By having the bootstrap managed by the DIC, it becomes a lot easier to use.
    Also, the XML file is way more maintainable than the annoying manual
    construction of the object graph.
    
    Sample usage:
    
        $collectionBuilder = $container->get('simple_php_easy_plus.number_collection_builder');
        $engine = $container->get('simple_php_easy_plus.engine');
        $runner = $container->get('simple_php_easy_plus.runner');
    
        $collectionBuilder->add('1');
        $collectionBuilder->add('2');
    
        $calcul = new Calcul($engine, $collectionBuilder->resolve());
        $runner->run($calcul);
    
        $value = $calcul->getResult()->getValue();

---
+++ src/SimplePHPEasyPlus/Bundle/SimplePHPEasyPlusBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SimplePHPEasyPlus\Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SimplePHPEasyPlusBundle extends Bundle
+{
+}
---
+++ tests/Bundle/ExtensionTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace SimplePHPEasyPlus\Bundle;
+
+use SimplePHPEasyPlus\Bundle\DependencyInjection\SimplePHPEasyPlusExtension;
+use SimplePHPEasyPlus\Calcul\Calcul;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class ExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testContainerConfig()
+    {
+        $extension = new SimplePHPEasyPlusExtension();
+
+        $container = new ContainerBuilder();
+        $extension->load(array(), $container);
+
+        $collectionBuilder = $container->get('simple_php_easy_plus.number_collection_builder');
+        $this->assertInstanceOf('SimplePHPEasyPlus\Number\NumberCollectionBuilder', $collectionBuilder);
+
+        $engine = $container->get('simple_php_easy_plus.engine');
+        $this->assertInstanceOf('SimplePHPEasyPlus\Engine', $engine);
+
+        $runner = $container->get('simple_php_easy_plus.runner');
+        $this->assertInstanceOf('SimplePHPEasyPlus\Calcul\CalculRunner', $runner);
+
+        $collectionBuilder->add('1');
+        $collectionBuilder->add('2');
+
+        $calcul = new Calcul($engine, $collectionBuilder->resolve());
+        $runner->run($calcul);
+
+        $this->assertEquals(3, $calcul->getResult()->getValue());
+    }
+}
